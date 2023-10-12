17726d83513c526f2aa810efbe38c222fdbb1e4c
Eliudcampos <131806393+Eliudcampos@users.noreply.github.com>
2023-10-11 19:46:32 -0500

    Add files via upload
    DAO CRUD

--- TiendaOnline/src/main/java/co/edu/tiendaonline/data/dao/concrete/sqlserver/TipoIdentificacionSQLServerDAO.java
+++ TiendaOnline/src/main/java/co/edu/tiendaonline/data/dao/concrete/sqlserver/TipoIdentificacionSQLServerDAO.java
@@ -1,10 +1,17 @@
 package co.edu.tiendaonline.data.dao.concrete.sqlserver;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import co.edu.tiendaonline.crosscutting.exception.concrete.CrossCuttingTiendaOnlineException;
+import co.edu.tiendaonline.crosscutting.exception.concrete.DataTiendaOnlineException;
+import co.edu.tiendaonline.crosscutting.messages.CatalogoMensajes;
+import co.edu.tiendaonline.crosscutting.messages.enumerator.CodigoMensaje;
 import co.edu.tiendaonline.data.dao.TipoIdentificacionDAO;
 import co.edu.tiendaonline.data.dao.base.SQLDAO;
 import co.edu.tiendaonline.data.entity.TipoIdentificacionEntity;
@@ -17,14 +24,58 @@ public final class TipoIdentificacionSQLServerDAO extends SQLDAO implements Tipo
 	}
 
 	@Override
-	public final void crear(TipoIdentificacionEntity tipoIdentificacion) {
-		// TODO Auto-generated method stub
+	public final void crear(TipoIdentificacionEntity entity) {
+		final StringBuilder sentencia = new StringBuilder();
 		
+		sentencia.append("INSERT INTO TipoIdentificacion (id, codigo, nombre, estado) ");
+		sentencia.append("VALUES (?, ?, ?, ?)");)
+		
+		try(final var sentenciaPreparada = getConexion().prepareStatement(sentencia.toString())){
+			
+			sentenciaPreparada.setObject(1, entity.getId());
+			sentenciaPreparada.setString(2, entity.getCodigo());
+			sentenciaPreparada.setString(3, entity.getNombre());
+			sentenciaPreparada.setBoolean(4, entity.isEstado());
+			
+			sentenciaPreparada.executeUpdate();
+			
+		}catch (final SQLException excepcion) {
+			var mensajeUsuario = "Se ha presentado un problema tratando de registar el tipo de identificacion";
+			var mensajeTecnico = "Se ha presentado un problema de tipo SQLException en el metodo crear de la clase TipoIdentificacionSQLServer tratando de llevar un registro de un nuevo tipo de identificacion, porfavor revise la traza completa del error presentado para asi poder identificar que sucerdio";
+			throw DataTiendaOnlineException.crear(excepcion, mensajeUsuario, mensajeTecnico);
+		}catch (final Exception excepcion) {
+			var mensajeUsuario = "Se ha presentado un problema tratando de registar el tipo de identificacion";
+			var mensajeTecnico = "Se ha presentado un problema inesperado de tipo Exception en el metodo crear de la clase TipoIdentificacionSQLServer tratando de llevar un registro de un nuevo tipo de identificacion, porfavor revise la traza completa del error presentado para asi poder identificar que sucerdio";
+			throw DataTiendaOnlineException.crear(excepcion, mensajeUsuario, mensajeTecnico);
+		}
 	}
 
 	@Override
-	public final void modificar(TipoIdentificacionEntity tipoIdentificacion) {
-		// TODO Auto-generated method stub
+	public final void modificar(TipoIdentificacionEntity entity) {
+
+		final StringBuilder sentencia = new StringBuilder();
+		
+		sentencia.append("INSERT INTO TipoIdentificacion (id, codigo, nombre, estado) ");
+		sentencia.append("VALUES (?, ?, ?, ?)");
+		
+		try(final var sentenciaPreparada = getConexion().prepareStatement(sentencia.toString())){
+			
+			sentenciaPreparada.setObject(1, entity.getId());
+			sentenciaPreparada.setString(2, entity.getCodigo());
+			sentenciaPreparada.setString(3, entity.getNombre());
+			sentenciaPreparada.setBoolean(4, entity.isEstado());
+			
+			sentenciaPreparada.executeUpdate();
+			
+		}catch (final SQLException excepcion) {
+			var mensajeUsuario = "Se ha presentado un problema tratando de registar el tipo de identificacion";
+			var mensajeTecnico = "Se ha presentado un problema de tipo SQLException en el metodo crear de la clase TipoIdentificacionSQLServer tratando de llevar un registro de un nuevo tipo de identificacion, porfavor revise la traza completa del error presentado para asi poder identificar que sucerdio";
+			throw DataTiendaOnlineException.crear(excepcion, mensajeUsuario, mensajeTecnico);
+		}catch (final Exception excepcion) {
+			var mensajeUsuario = "Se ha presentado un problema tratando de registar el tipo de identificacion";
+			var mensajeTecnico = "Se ha presentado un problema inesperado de tipo Exception en el metodo crear de la clase TipoIdentificacionSQLServer tratando de llevar un registro de un nuevo tipo de identificacion, porfavor revise la traza completa del error presentado para asi poder identificar que sucerdio";
+			throw DataTiendaOnlineException.crear(excepcion, mensajeUsuario, mensajeTecnico);
+		}
 		
 	}
 
@@ -35,8 +86,52 @@ public final class TipoIdentificacionSQLServerDAO extends SQLDAO implements Tipo
 	}
 
 	@Override
-	public final Optional<TipoIdentificacionEntity> consultarPorId(UUID id) {
-		// TODO Auto-generated method stub
+	public final Optional<TipoIdentificacionEntity> consultarPorId(final UUID id) {
+		
+		final var sentencia = new StringBuilder();		
+		sentencia.append("SELECT id, codigo, nombre, estado ");
+		sentencia.append("FROM TipoIdentificacion ");
+		sentencia.append("WHERE id = ? ");
+		
+		Optional<TipoIdentificacionEntity> resultado = Optional.empty();
+		
+		try(final var sentenciaPreparada = getConexion().prepareStatement(sentencia.toString())){
+			
+			sentenciaPreparada.setObject(1, id);
+			
+			try(final var resultados = sentenciaPreparada.executeQuery()){
+				if(resultados.next()) {
+					var tipoIdentificacionEntity = TipoIdentificacionEntity.crear(UUID.fromString(resultados.getObject("id").toString()), resultados.getString("codigo"), resultados.getString("nombre"), resultados.getBoolean("estado"));
+				
+					resultado = Optional.of(tipoIdentificacionEntity);
+				}
+			}catch (final SQLException excepcion) {
+				
+				var mensajeUsuario = "Se ha presentado un problema tratando de consultar la informacion del tipo de identificación por el identificador deseado";
+				var mensajeTecnico = "Se ha presentado un problema de tipo SQLException en el metodo consultar el tipo de identificacion por el identificador deseado, porfavor revise la traza completa del error presentado para asi poder identificar que sucerdio";
+				throw DataTiendaOnlineException.crear(excepcion, mensajeUsuario, mensajeTecnico);
+				
+			}
+			
+		
+		
+		}catch (final SQLException excepcion) {			
+			var mensajeUsuario = "Se ha presentado un problema tratando de consultar la informacion del tipo de identificación por el identificador deseado";
+			var mensajeTecnico = "Se ha presentado un problema de tipo SQLException en el metodo consultar el tipo de identificacion por el identificador deseado, porfavor revise la traza completa del error presentado para asi poder identificar que sucerdio";
+			throw DataTiendaOnlineException.crear(excepcion, mensajeUsuario, mensajeTecnico);
+
+		}catch (final DataTiendaOnlineException excepcion) {			
+			var mensajeUsuario = "Se ha presentado un problema tratando de consultar la informacion del tipo de identificación por el identificador deseado";
+			var mensajeTecnico = "Se ha presentado un problema de tipo SQLException en el metodo consultar el tipo de identificacion por el identificador deseado, porfavor revise la traza completa del error presentado para asi poder identificar que sucerdio";
+			throw DataTiendaOnlineException.crear(excepcion, mensajeUsuario, mensajeTecnico);
+			
+		
+		}catch (final Exception excepcion) {
+			var mensajeUsuario = "Se ha presentado un problema tratando de consultar la informacion del tipo de identificación por el identificador deseado";
+			var mensajeTecnico = "Se ha presentado un problema inesperado de tipo Exception en el metodo consultar el tipo de identificacion por el identificador deseado, porfavor revise la traza completa del error presentado para asi poder identificar que sucerdio";
+			throw DataTiendaOnlineException.crear(excepcion, mensajeUsuario, mensajeTecnico);
+		}
+		
 		return Optional.empty();
 	}
 
